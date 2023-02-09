bc3f685efcbab3703b3f32ac89f297b93f979b70
Keaton J. Burns <keaton.burns@gmail.com>
2023-02-09 10:05:05 -0500

    Change group array dtype to support floats

--- dedalus/core/distributor.py
+++ dedalus/core/distributor.py
@@ -415,7 +415,7 @@ class Layout:
     def _group_arrays(self, elements, domain):
         # Convert to groups basis-by-basis
         grid_space = self.grid_space
-        groups = np.zeros_like(elements)
+        groups = np.zeros(elements.shape, dtype=np.float64)
         groups = np.ma.masked_array(groups)
         for basis in domain.bases:
             basis_axes = slice(self.dist.first_axis(basis), self.dist.last_axis(basis)+1)
